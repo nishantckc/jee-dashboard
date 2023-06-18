7c95e2b5b910da3ead296ae7535af6403b6c47af
nishantckc <nishantckc@gmail.com>
2023-06-18 16:08:24 +0530

    Update settings.py

--- backend/dashboard/settings.py
+++ backend/dashboard/settings.py
@@ -147,5 +147,5 @@ REST_FRAMEWORK = {
                 'rest_framework.renderers.JSONRenderer',
                  ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
+    'PAGE_SIZE': 500
 }
